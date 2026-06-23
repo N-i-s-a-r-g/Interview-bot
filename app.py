a2b61ec4be51bb877d55e5d1ebd1961bb4955afc
N-i-s-a-r-g <manoharnisarg@gmail.com>
2026-06-23 18:01:01 +0530

    Add user initialization in session state
    Initialize 'user' in session state if not present.

--- app.py
+++ app.py
@@ -42,6 +42,9 @@ users = {
 if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
     
+if "user" not in st.session_state:
+    st.session_state.user = None
+    
 if "saved" not in st.session_state:
     st.session_state.saved = False
     
